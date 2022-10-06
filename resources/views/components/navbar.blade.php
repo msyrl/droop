0014618528fe4257416ea2bd53630aebf6040686
Muhammad Syachrul <syachrulmuhammad@gmail.com>
2022-10-06 11:38:09 +0700

    fix(navbar): remove notification

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,49 +13,6 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-        <!-- Notifications Dropdown Menu -->
-        <li class="nav-item dropdown">
-            <a
-                class="nav-link"
-                data-toggle="dropdown"
-                href="#"
-            >
-                <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge">15</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">15 Notifications</span>
-                <div class="dropdown-divider"></div>
-                <a
-                    href="#"
-                    class="dropdown-item"
-                >
-                    <span>4 new messages</span>
-                    <span class="float-right text-muted text-sm">3 mins</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a
-                    href="#"
-                    class="dropdown-item"
-                >
-                    <span>8 friend requests</span>
-                    <span class="float-right text-muted text-sm">12 hours</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a
-                    href="#"
-                    class="dropdown-item"
-                >
-                    <span>3 new reports</span>
-                    <span class="float-right text-muted text-sm">2 days</span>
-                </a>
-                <div class="dropdown-divider"></div>
-                <a
-                    href="#"
-                    class="dropdown-item dropdown-footer"
-                >See All Notifications</a>
-            </div>
-        </li>
         <li class="nav-item dropdown">
             <a
                 class="nav-link"
